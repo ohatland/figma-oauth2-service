c267af93720fc0bcd7916b7b7e6d73e871682a84
Øyvind Hatland <oyvind.hatland@yahoo.com>
2024-02-14 16:51:17 +0100

    add keyvalue list to create and find accesskey

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 
@@ -11,20 +14,37 @@ import (
 	"golang.org/x/oauth2"
 )
 
+type UserToken struct {
+	AccessToken string
+	ReadKey     string // used to find the stored token in memory
+	WriteKey    string // used as state value for OAuth2
+}
+
+type GeneratedKeyPairResponse struct {
+	ReadKey  string `json:"readKey"`
+	WriteKey string `json:"writeKey"`
+}
+
+type AccessTokenResponse struct {
+	AccessToken string `json:"accessToken"`
+}
+
 var (
 	// Configure your OAuth2 settings here
 	oauth2Config = &oauth2.Config{
 		ClientID:     "",
 		ClientSecret: "",
 		RedirectURL:  "http://localhost:8080/callback",
-		Scopes:       []string{"files:read"}, // Specify the scopes you need
+		Scopes:       []string{"file_variables:read"},
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  "https://www.figma.com/oauth",
 			TokenURL: "https://www.figma.com/api/oauth/token",
 		},
 	}
-	// Random string for state verification (should be more complex in production)
-	oauthStateString = "random"
+
+	userTokenList []UserToken
+
+	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 )
 
 func main() {
@@ -36,32 +56,83 @@ func main() {
 	oauth2Config.ClientID = os.Getenv("CLIENT_ID")
 	oauth2Config.ClientSecret = os.Getenv("CLIENT_SECRET")
 
+	/*
+		Order of operations:
+		1. User generates keypair with /keypair
+		2. User logs in with /login?writeKey=writeKey
+		3. User gets token with /token?readKey=readKey
+		4. User can now use the token to make requests to Figma's API
+
+		Note: The keypair is stored in memory and will be lost if the server is restarted
+	*/
+
+	http.HandleFunc("/keypair", handleKeypair)
 	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/token", handleToken)
 	http.HandleFunc("/callback", handleCallback)
 	fmt.Println("Started running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handleLogin(w http.ResponseWriter, r *http.Request) {
-	// Use random string from the user as state
-	oauthStateString = r.FormValue("state")
-	if oauthStateString == "" {
-		oauthStateString = "random"
+// create keypair for user, store it in memory and return it to the user
+func handleKeypair(w http.ResponseWriter, r *http.Request) {
+
+	userToken := generateKeyPair()
+
+	response := GeneratedKeyPairResponse{
+		ReadKey:  userToken.ReadKey,
+		WriteKey: userToken.WriteKey,
 	}
-	// Redirect user to consent page to ask for permission
-	url := oauth2Config.AuthCodeURL(oauthStateString)
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// redirects user to Figma's OAuth2 login page
+// user needs to already have a keypair
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	writeKey := r.FormValue("writeKey")
+	if !checkIfWriteKeyExistsInMemory(writeKey) {
+		fmt.Fprintln(w, "Invalid write key")
+		return
+	}
+	url := oauth2Config.AuthCodeURL(writeKey)
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
-func handleCallback(w http.ResponseWriter, r *http.Request) {
-	state := r.FormValue("state")
-	if state != oauthStateString {
-		fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+// user retrive token for the stored keypair
+func handleToken(w http.ResponseWriter, r *http.Request) {
+	readKey := r.FormValue("readKey")
+
+	accessToken, err := findAccessTokenInMemory(readKey)
+	if err != nil {
+		fmt.Fprintln(w, err)
 		return
 	}
 
+	response := AccessTokenResponse{
+		AccessToken: accessToken,
+	}
+
+	// TODO handle error
+	removeAccessTokenFromMemory(accessToken)
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
+
+// retrive token from Figmas OAuth2 and store it in memory
+func handleCallback(w http.ResponseWriter, r *http.Request) {
+	state := r.FormValue("state")
 	code := r.FormValue("code")
+
 	token, err := oauth2Config.Exchange(context.Background(), code)
 	if err != nil {
 		fmt.Printf("oauthConf.Exchange() failed with '%s'\n", err)
@@ -69,7 +140,88 @@ func handleCallback(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Got token: %v\n", token)
+	if err := storeUserTokenInMemory(state, token); err != nil {
+		fmt.Printf("%s\n", err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
 
-	fmt.Println("Token: ", token)
+	fmt.Fprintln(w, "Got token")
+}
+
+func generateKeyPair() UserToken {
+	userToken := UserToken{
+		AccessToken: "",
+		ReadKey:     generateRandomString(64),
+		WriteKey:    generateRandomString(64),
+	}
+	userTokenList = append(userTokenList, userToken)
+	return userToken
+}
+
+func storeUserTokenInMemory(writeKey string, token *oauth2.Token) error {
+	keyStored := false
+	for i, userToken := range userTokenList {
+		if userToken.WriteKey == writeKey {
+			userTokenList[i].AccessToken = token.AccessToken
+			keyStored = true
+			return nil
+		}
+	}
+	if !keyStored {
+		return errors.New("could not find state value for received token")
+	}
+
+	return nil
+}
+
+// find oauth2 token in memory with readKey
+func findAccessTokenInMemory(readKey string) (string, error) {
+	for _, userToken := range userTokenList {
+		if userToken.ReadKey == readKey {
+			if userToken.AccessToken == "" {
+				// when key pair is generated, and the access token is not yet created
+				return "", errors.New("could not find AccessToken for given readKey")
+			}
+			return userToken.AccessToken, nil
+		}
+	}
+
+	// when key pair does not exist
+	return "", errors.New("could not find AccessToken for given readKey")
+}
+
+func checkIfWriteKeyExistsInMemory(writeKey string) bool {
+	for _, userToken := range userTokenList {
+		if userToken.WriteKey == writeKey {
+			return true
+		}
+	}
+	return false
+}
+
+func removeAccessTokenFromMemory(accessToken string) error {
+	indexToRemove := -1
+	for i, userToken := range userTokenList {
+		if userToken.AccessToken == accessToken {
+			indexToRemove = i
+			break
+		}
+	}
+
+	userTokenList = append(userTokenList[:indexToRemove], userTokenList[indexToRemove+1:]...)
+
+	if indexToRemove == -1 {
+		return errors.New("could not find token in memory")
+	}
+	return nil
+}
+
+// generateRandomString returns a random string of n characters
+func generateRandomString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
